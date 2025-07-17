1e1a9c5083d8efbcf17ddc9e3a5ff5c044744823
Rubem Ernesto <rubemernesto2@gmail.com>
2025-07-17 11:17:51 +0100

    some correction

--- src/components/CprojectLink.tsx
+++ src/components/CprojectLink.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
 
 export default function CprojectLink () {
-    return  <Link href={"/UserPages/CreateProject"} className="px-4 py-2 bg-blue-500 rounded-[7px] text-white font-bold animate-bounce animate-infinite">Criar um agora!</Link>
+    return  <Link href={"/UserPages/CreateProject"} className="px-4 py-2 bg-blue-500 rounded-[7px] text-white font-bold animate-bounce animate-infinite">Criar um Projecto agora!</Link>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Contact() {
             <div className="flex flex-col justify-center items-center gap-10 h-full">
                 <div className="flex flex-col justify-center items-center gap-2.5" data-aos="fade-right">
                   <h2 className="text-blue-500 text-5xl font-bold ">Bem-Vindo ao Projects <span className="text-6xl text-amber-500">Mananger</span></h2>
-                  <p>Paneje, organize e crie já projectos de maneira eficaz com a nossa app!</p>
+                  <p>Planeje, organize e crie já projectos de maneira eficaz com a nossa app!</p>
                   <p className="pb-5">Ferramenta criada com ❤️‍🩹 por @Rubem-Ernesto-Figueiredo</p>
                   <CprojectLink/>
                 </div>
